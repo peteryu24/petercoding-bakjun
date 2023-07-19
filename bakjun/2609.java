34bba30ac17b414b2aaa08d6ced8df3c97186703
DudeFromKorea <peteryu24@naver.com>
2023-07-19 19:55:48 +0900

    Update 2609.java

--- bakjun/2609.java
+++ bakjun/2609.java
@@ -15,7 +15,7 @@ public class Main {
 		System.out.println(a * b / d);
 	}
  
-	public static int gcd(int a, int b) {
+	public static int gcd(int a, int b) { // 
  
 		while (b != 0) {
 			int r = a % b; 
