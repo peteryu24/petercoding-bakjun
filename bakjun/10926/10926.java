9cc448cd880e7c2a81a8a293ac580fa20284e904
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:25:28 +0900

    Create 10926.java

---
+++ bakjun/10926/10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 10926{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+         
+        String id = scan.next();
+        scan.close();
+        System.out.println(id+"??!");
+    }
+}
