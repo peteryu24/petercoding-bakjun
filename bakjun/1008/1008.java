abd1f2bbf63b2a2edf558e81d8f42d320d8f6276
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:22:34 +0900

    Create 1008.java

---
+++ bakjun/1008/1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 1008{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        scan.close();
+        System.out.println(a/b);
+    }
+}
