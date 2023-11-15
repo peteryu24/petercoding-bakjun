7cbaf6ca412e3b27b0276f334b2bc247fcc74890
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:34:44 +0900

    int - num sysout

--- bakjun/18108/18108.java
+++ bakjun/18108/18108.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class 18108{
-    public static void main(String [] args){
+    public static void main(String [] args){ 
         Scanner scan = new Scanner(System.in);
          
         int year = scan.nextInt();
