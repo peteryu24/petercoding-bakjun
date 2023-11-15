7b8071a32838252ef72927993133135ba0ee9d1b
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:18:52 +0900

    Update and rename bakjun/2609.java to bakjun/2609/2609.java

--- bakjun/2609/2609.java
+++ bakjun/2609/2609.java
@@ -2,7 +2,7 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
-public class Main {
+public class 2609 {
 	public static void main(String[] args) {
  
 		Scanner in = new Scanner(System.in);
