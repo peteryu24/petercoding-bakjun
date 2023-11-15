558b56354264c8d12c5038f475c87783ac3a4761
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:33:47 +0900

    a + b, a - b, a * b, a / b, a % b

--- bakjun/10869/10869.java
+++ bakjun/10869/10869.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class 10869{
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+        int a = scan.nextInt(); 
         int b = scan.nextInt();
         scan.close();
         
