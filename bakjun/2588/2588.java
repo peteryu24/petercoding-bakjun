4b486be76a67c768b37010d3e5d28a1b8bb14bd8
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:31:37 +0900

    Create 2588.java

---
+++ bakjun/2588/2588.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class 2588{
+ 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+ 
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+        
+		scan.close();
+ 
+		System.out.println(a*(b%10));
+		System.out.println(a*(b%100/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+	}
+ 
+}
