2aa7e6506e9bb6db5ed25de5285c52ca3a851b33
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:32:20 +0900

    a + b

--- bakjun/1000/1000.java
+++ bakjun/1000/1000.java
@@ -4,7 +4,7 @@ public class 1000 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         
-        int a = scan.nextInt();
+        int a = scan.nextInt(); 
         int b = scan.nextInt();
         
         scan.close();
