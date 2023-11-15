49a0651e3d36f8359142c168c27909e8570ce78f
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:32:31 +0900

    a - b

--- bakjun/1001/1001.java
+++ bakjun/1001/1001.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class 1001 {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
         
         int a = scan.nextInt();
         int b = scan.nextInt();
