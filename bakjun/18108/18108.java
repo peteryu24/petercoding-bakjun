29317973201a7305ae314894c8509c4f2fbaa093
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:28:32 +0900

    Create 18108.java

---
+++ bakjun/18108/18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 18108{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+         
+        int year = scan.nextInt();
+        scan.close();
+        System.out.println(year-543);
+    }
+}
