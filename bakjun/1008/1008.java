21a1ad1d640b327dc96f8dd792bbe39ea1e346c8
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:32:43 +0900

    a / b

--- bakjun/1008/1008.java
+++ bakjun/1008/1008.java
@@ -4,7 +4,7 @@ public class 1008{
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
         double a = scan.nextDouble();
-        double b = scan.nextDouble();
+        double b = scan.nextDouble(); 
         scan.close();
         System.out.println(a/b);
     }
