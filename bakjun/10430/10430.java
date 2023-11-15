5871fdfdb5eea0bc06d2a6c0f8c80d886923fe37
DudeFromKorea <peteryu24@naver.com>
2023-11-15 13:42:59 +0900

    (A+B)%C, ((A%C) + (B%C))%C, (A×B)%C,  ((A%C) × (B%C))%C

--- bakjun/10430/10430.java
+++ bakjun/10430/10430.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class 10430{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
         
         int a = scanner.nextInt();
         int b = scanner.nextInt();
