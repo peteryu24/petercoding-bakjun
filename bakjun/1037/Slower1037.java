dd22eeae4f7c7640b33d8be1b61d896fd3b007ce
DudeFromKorea <peteryu24@naver.com>
2023-11-16 16:09:51 +0900

    get Num by divisor with array (slow version)

---
+++ bakjun/1037/Slower1037.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Slower1037 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int cnt = scan.nextInt();
+		int arr[] = new int[cnt];
+		int answer = 0;
+		for (int i = 0; i < cnt; i++) {
+			int num = scan.nextInt();
+			arr[i] = num;
+		}
+		scan.close();
+
+		Arrays.sort(arr);
+		answer = arr[0] * arr[cnt - 1];
+
+		System.out.println(answer);
+	}
+}
