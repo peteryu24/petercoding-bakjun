127958c7c0946f7f3cf9aa42b259e95b9229cb51
DudeFromKorea <peteryu24@naver.com>
2023-11-16 11:38:53 +0900

    cal sum as many as the writer wants(show the numbers also)

---
+++ bakjun/11022/11022.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class 11022 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int cnt = scan.nextInt();
+		for (int i = 1; i <= cnt; i++) {
+			int firstNum = scan.nextInt();
+			int secondNum = scan.nextInt();
+
+			System.out.println("Case #" + i + ": " + firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+		}
+		scan.close();
+
+	}
+}
