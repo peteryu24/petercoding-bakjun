f3ab8549cf4077aa297ae40e2b3b98ee6e87af44
DudeFromKorea <peteryu24@naver.com>
2023-11-16 17:42:30 +0900

    get an alphabet and print ASCII

---
+++ bakjun/11654/11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class 11654 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		char letter = scan.next().charAt(0);
+		scan.close();
+
+		System.out.println((int) letter);
+
+	}
+}
