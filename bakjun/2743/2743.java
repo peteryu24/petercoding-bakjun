5867032ffe4f68e61b3116b8f8d70b52f5df843e
DudeFromKorea <peteryu24@naver.com>
2023-11-16 17:28:55 +0900

    get the length of word

---
+++ bakjun/2743/2743.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class 2743 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+		scan.close();
+		System.out.println(word.length());
+
+	}
+}
