eb3f08c5c4963cce977f6c8f060512d9df32d1a4
DudeFromKorea <peteryu24@naver.com>
2023-11-16 17:54:56 +0900

    get a word and make a list of alphabet checker
    apple
    0 -1 -1 -1 4 -1 -1 -1 -1 -1 -1 3 -1 -1 -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1

--- bakjun/10809/10809.java
+++ bakjun/10809/10809.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class 10809 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		Scanner scan = new Scanner(System.in);
 
 		int[] arr = new int[26];
