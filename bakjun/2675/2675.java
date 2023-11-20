4d60df37fb5e4ae2a55b26a5a9f8a8963088d63f
DudeFromKorea <peteryu24@naver.com>
2023-11-20 16:50:23 +0900

    print a word by char as many as the user wants

---
+++ bakjun/2675/2675.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class 2675 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int tryCnt = scan.nextInt();
+        for(int i=0; i<tryCnt; i++) {
+        	int times = scan.nextInt();
+        	String word = scan.next();
+        	int wordLength = word.length();
+        	for(int j=0; j<wordLength; j++) {
+        		for(int k=0; k<times; k++) {
+        			System.out.print(word.charAt(j));
+        		}
+        	}  	
+        }
+        scan.close();
+    }
+}
