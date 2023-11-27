3a2a6d6fd96df92c0ca22fc9e5465c969b77ffbf
DudeFromKorea <peteryu24@naver.com>
2023-11-27 14:38:45 +0900

    print what it read. hasNext(), nextLine()

---
+++ bakjun/11718/11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class 11718 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while(scan.hasNext()) {
+			System.out.println(scan.nextLine());
+		}
+		scan.close();
+
+	}
+}
