9f97c5ee358bebdf75a87dd57406d49ddd5eaf75
DudeFromKorea <peteryu24@naver.com>
2023-11-27 14:59:10 +0900

    print pyramid - revers with for
        *
       ***
      *****
     *******
    *********
     *******
      *****
       ***
        *

---
+++ bakjun/2444/2444.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class 2444 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int i, j, k, l;
+		sc.close();
+
+		for (i = 0; i < n; i++) {
+			for (j = i + 1; j < n; j++) {
+				System.out.print(" ");
+			}
+			for (k = i; k >= 0; k--) { 
+				System.out.print("*");
+			}
+			for (l = i; l > 0; l--) { 
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (i = 1; i < n; i++) {
+			for (j = i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for (k = i; k < n; k++) { 
+				System.out.print("*");
+			}
+			for (l = i + 1; l < n; l++) { 
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
