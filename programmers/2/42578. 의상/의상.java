01acd16c0dab1d491618d673384c99e1c2123c7a
DudeFromKorea <peteryu24@naver.com>
2023-12-13 23:33:43 +0900

    formatting

--- programmers/2/42578. 의상/의상.java
+++ programmers/2/42578. 의상/의상.java
@@ -3,17 +3,17 @@ import java.util.Map;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> clothesMap = new HashMap<>();
+        Map<String, Integer> wardrobe = new HashMap<>();
 
         for (String[] cloth : clothes) {
-            String type = cloth[1];
-            clothesMap.put(type, clothesMap.getOrDefault(type, 0) + 1);
+            String category = cloth[1];
+            wardrobe.put(category, wardrobe.getOrDefault(category, 0) + 1);
         }
 
         int answer = 1;
 
-        for (int count : clothesMap.values()) {
-            answer *= (count + 1);
+        for (int typeCount : wardrobe.values()) {
+            answer *= (typeCount + 1);
         }
 
         return answer - 1;
