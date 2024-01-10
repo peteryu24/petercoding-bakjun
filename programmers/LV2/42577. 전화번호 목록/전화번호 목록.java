4270bfafd6c8db3f5d363a10a784b2f55673e7cd
DudeFromKorea <peteryu24@naver.com>
2024-01-10 11:00:23 +0900

    Update 전화번호 목록.java

--- programmers/LV2/42577. 전화번호 목록/전화번호 목록.java
+++ programmers/LV2/42577. 전화번호 목록/전화번호 목록.java
@@ -10,11 +10,12 @@ class Solution {
 		}
 
 		for (int i = 0; i < phoneBookLength; i++) {
+			
 			int phoneBookItemLength = phoneBook[i].length();
+			
 			for (int j = 1; j < phoneBookItemLength; j++) {
-				if (phoneSet.contains(phoneBook[i].substring(0, i))) {
+				if (phoneSet.contains(phoneBook[i].substring(0, i)))
 					return false;
-				}
 			}
 		}
 		return true;
