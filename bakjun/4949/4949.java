f129e1bb771bef211a7c3e8e097d57df4e300190
DudeFromKorea <67302252+peteryu24@users.noreply.github.com>
2024-09-18 17:10:37 +0900

    Update 4949.java

--- bakjun/4949/4949.java
+++ bakjun/4949/4949.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Stack;
 
-public class Main {
+public class 4949 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input;
